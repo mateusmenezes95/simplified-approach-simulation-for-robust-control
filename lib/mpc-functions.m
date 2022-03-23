7feb658cc69abd52dfd967709c6ee2dcf17e7542
Mateus Menezes <mateusmenezes95@gmail.com>
2022-03-23 00:05:29 -0300

    Keep last future ref when its over

--- lib/mpc-functions.m
+++ lib/mpc-functions.m
@@ -38,6 +38,10 @@ endfunction
 function horizon_refs = get_horizon_references(k, prediction_horizon, future_refs)
   horizon_refs = [];
   for j=k:k+prediction_horizon-1
-    horizon_refs = [horizon_refs; future_refs(:,j)];
+    if j > length(future_refs)
+      horizon_refs = [horizon_refs; future_refs(:,end)];  % Keeps the last reference
+    else
+      horizon_refs = [horizon_refs; future_refs(:,j)];
+    endif
   endfor
 endfunction
