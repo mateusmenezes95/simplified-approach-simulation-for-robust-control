9c9e7d7d57fece206b69bf65e8e989f65853b787
Mateus Menezes <mateusmenezes95@gmail.com>
2022-03-24 23:06:19 -0300

    Add generation of square trajectory

--- lib/mpc-functions.m
+++ lib/mpc-functions.m
@@ -45,3 +45,24 @@ function horizon_refs = get_horizon_references(k, prediction_horizon, future_ref
     endif
   endfor
 endfunction
+
+% Future references must be in Y = [yr(k) yr(k+1) ... yr(k+N-1)]
+function horizon_refs = get_square_trajectory_horizon_references(k, actual_pose, prediction_horizon, waypoints, nav_vel)
+  x=1;
+  y=2;
+  theta=3;
+  horizon_refs = [];
+  for j=k:k+prediction_horizon-1
+    if j > length(waypoints)
+      phi = atan2(waypoints(y,end)-actual_pose(y), waypoints(x,end)-actual_pose(x))  # atan2(yr(k + j|k) - yr(k), xr(k + j|k) − xr(k))
+      temp_vec = [nav_vel*cos(phi) nav_vel*sin(phi) (waypoints(theta,end) - actual_pose(theta))]';
+      temp_vec = rotz(actual_pose(theta))*temp_vec;
+      horizon_refs = [horizon_refs; temp_vec];
+    else
+      phi = atan2(waypoints(y,j)-actual_pose(y), waypoints(x,j)-actual_pose(x))  # atan2(yr(k + j|k) - yr(k), xr(k + j|k) − xr(k))
+      temp_vec = [nav_vel*cos(phi) nav_vel*sin(phi) (waypoints(theta,j) - actual_pose(theta))]';
+      temp_vec = rotz(actual_pose(theta))*temp_vec
+      horizon_refs = [horizon_refs; temp_vec];
+    endif
+  endfor
+endfunction
--- lib/trajectory-functions.m
+++ lib/trajectory-functions.m
@@ -18,3 +18,23 @@ function [x_points, y_points, v] = generate_circle_trajectory(radius, v_nav, t)
     endif
   endfor
 endfunction
+
+function waypoints = generate_square_trajectory(square_size, nav_vel, sampling_period)
+  path_nav_time = (square_size*4)/nav_vel;
+  waypoints_qty = round(path_nav_time/sampling_period);
+  waypoint_step = square_size/waypoints_qty;
+
+  x1 = linspace(0, square_size, waypoints_qty);
+  theta = [zeros(1,length(x1))'];
+  y1 = linspace(waypoint_step, square_size, waypoints_qty-1);
+  theta = [theta; deg2rad(ones(1,length(y1))*90)'];
+  x2 = linspace(square_size-waypoint_step, 0, waypoints_qty-1);
+  theta = [theta; deg2rad(ones(1,length(x2))*180)'];
+  y2 = linspace(square_size-waypoint_step, waypoint_step, waypoints_qty-2);
+  theta = [theta; deg2rad(ones(1,length(y2))*-135)'];
+
+  x = [x1'; ones(1,length(y1))'; x2'; zeros(1, length(y2))'];
+  y = [zeros(1, length(x1))'; y1'; ones(1, length(x2))'; y2'];
+
+  waypoints = [x y theta]';
+endfunction
