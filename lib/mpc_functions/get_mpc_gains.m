2adb0a04da4736e95da7888fa1941e376f85ce54
Bruno Schettini <brschettini@gmail.com>
2022-03-31 22:23:55 -0300

    Create kao_criteria script

--- lib/mpc_functions/get_mpc_gains.m
+++ lib/mpc_functions/get_mpc_gains.m
@@ -4,6 +4,5 @@ function [Kw, Kmpc, Qaug, Raug] = get_mpc_gains(Ap, Bp, Cp, q, r, Np, Nu)
   common_factor = inv((Bp'*Cp'*Qaug*Cp*Bp)+Raug)*Bp'*Cp'*Qaug';
   Kw = common_factor; % Needs review
   
-  
   Kmpc = common_factor*Cp*Ap;
 end
