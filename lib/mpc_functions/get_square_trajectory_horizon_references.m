14d79318658bdc20b25ac6bba22a26c936f5a92c
Mateus Menezes <mateusmenezes95@gmail.com>
2022-03-31 23:01:59 -0300

    Update function to generate horizon references
    This function is equal the one used in the simulink matlab function block

--- lib/mpc_functions/get_square_trajectory_horizon_references.m
+++ lib/mpc_functions/get_square_trajectory_horizon_references.m
@@ -1,20 +1,36 @@
-function horizon_refs = get_square_trajectory_horizon_references(k, actual_pose, prediction_horizon, waypoints, nav_vel)
-% Future references must be in Y = [yr(k) yr(k+1) ... yr(k+N-1)]
-  x=1;
-  y=2;
-  theta=3;
-  horizon_refs = [];
-  for j=k:k+prediction_horizon-1
-    if j > length(waypoints)
-      phi = atan2(waypoints(y,end)-actual_pose(y), waypoints(x,end)-actual_pose(x))  % atan2(yr(k + j|k) - yr(k), xr(k + j|k) − xr(k))
-      temp_vec = [nav_vel*cos(phi) nav_vel*sin(phi) (waypoints(theta,end) - actual_pose(theta))]';
-      temp_vec = rotz(actual_pose(theta))*temp_vec;
-      horizon_refs = [horizon_refs; temp_vec];
-    else
-      phi = atan2(waypoints(y,j)-actual_pose(y), waypoints(x,j)-actual_pose(x))  % atan2(yr(k + j|k) - yr(k), xr(k + j|k) − xr(k))
-      temp_vec = [nav_vel*cos(phi) nav_vel*sin(phi) (waypoints(theta,j) - actual_pose(theta))]';
-      temp_vec = rotz(actual_pose(theta))*temp_vec
-      horizon_refs = [horizon_refs; temp_vec];
+function horizon_refs = get_square_trajectory_horizon_references(waypoints, nav_vel, k, Np, pose)
+    % Future references must be in Y = [yr(k) yr(k+1) ... yr(k+N-1)]
+    horizon_refs = zeros(Np*3,1);
+
+    k=k+1;
+
+    x=pose(1);
+    y=pose(2);
+    theta=pose(3);
+    tol = 0.01;
+
+    if (k > length(waypoints))
+        return
     end
-  end
+
+    i=1;
+    for j=k:k+Np-1
+        Rz = [cos(theta) sin(theta) 0; -sin(theta) cos(theta) 0; 0 0 1];
+
+        if j > length(waypoints)
+            x_ref = waypoints(1,end);
+            y_ref = waypoints(2,end);
+            theta_ref = waypoints(3,end);
+        else
+            x_ref = waypoints(1,j);
+            y_ref = waypoints(2,j);
+            theta_ref = waypoints(3,j);
+        end
+
+        phi = atan2(y_ref-y, x_ref-x);  % atan2(yr(k + j|k) - yr(k), xr(k + j|k) − xr(k))
+        temp_vec = [nav_vel*cos(phi) nav_vel*sin(phi) theta_ref-theta]';
+        temp_vec = Rz*temp_vec;
+        horizon_refs(i:i+2,1) = temp_vec;
+        i=i+3;
+    end
 end
