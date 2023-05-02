abfc09d81c309c1dfd002a1f72da5d97951a9573
Mateus Menezes <mateus.meneses@fieb.org.br>
2023-05-01 23:55:07 -0300

    Fix augmented matrices used in the LMI

--- scripts/lmi_h_inf_norm_computation.m
+++ scripts/lmi_h_inf_norm_computation.m
@@ -22,8 +22,10 @@ uncertainty_delta = 0.2;
 uncertainty_lower_bound = (1 - uncertainty_delta);
 uncertainty_upper_bound = (1 + uncertainty_delta);
 
-uncertainty_mass_vector = [nominal_mass*uncertainty_lower_bound, nominal_mass*uncertainty_upper_bound];
-uncertainty_inertia_vector = [nominal_inertia*uncertainty_lower_bound, nominal_inertia*uncertainty_upper_bound];
+uncertainty_mass_vector = [nominal_mass*uncertainty_lower_bound, ... 
+                           nominal_mass*uncertainty_upper_bound];
+uncertainty_inertia_vector = [nominal_inertia*uncertainty_lower_bound, ...
+                              nominal_inertia*uncertainty_upper_bound];
 
 % =============================================================================
 % State-space variables size
@@ -55,12 +57,12 @@ h_infnty_norm_vector = zeros(size(q));
 loop_index= 1;
 for q_sample = q
     fprintf('Computing H infinity norm for q = %d. Progress %.2f\n%', q_sample, (loop_index/size(q,2))*100);
-    norms_by_matlab = [];
+    % norms_by_matlab = [];
     ineqs=[];
     for i = 1:size(uncertainty_mass_vector, 2)
       for j = 1:size(uncertainty_inertia_vector, 2)
         % Augmented state-space due the integrator addition
-        [Aaug, Baug, Caug] = get_model_augmented_matrices(uncertainty_mass_vector(i), ...
+        [Aaug, Baug, Caug, A, B, C, D] = get_model_matrices(uncertainty_mass_vector(i), ...
                                 uncertainty_inertia_vector(j), sampling_period);
 
         % Matrices to estimate the future states and control signals
@@ -68,9 +70,18 @@ for q_sample = q
         [Kw, Kmpc, Q, R] = get_mpc_gains(Acal, Bcal, Ccal, q_sample, r, prediction_horizon, control_horizon);
         Kmpc = Kmpc(1:amount_of_inputs, :); % Receding horizon control
 
-        Almi = Aaug-Baug*Kmpc;
-        Blmi = Baug;
-        Clmi = -Kmpc;
+        z = tf('z', sampling_period);
+        C_z = -Kmpc*[eye(amount_of_states);(z/(z-1))*C];
+        Cz_ss = ss(C_z);
+
+        Ac = Cz_ss.A;
+        Bc = Cz_ss.B;
+        Cc = Cz_ss.C;
+        Dc = Cz_ss.D;
+
+        Almi = [A+B*Dc*C B*Cc zeros(3); Bc*C Ac zeros(3); Dc*C Cc zeros(3)];
+        Blmi = [B; zeros(3); zeros(3)];
+        Clmi = [Dc Cc -eye(3)];
         Dlmi = zeros(3);
 
         n = size(Almi,1); % dimensão do sistema aumentado
@@ -96,12 +107,31 @@ for q_sample = q
     h_infnty_norm = sqrt(mu);
     h_infnty_norm_vector(loop_index) = h_infnty_norm;
 
-    Nmax = floor(1/h_infnty_norm);
+    Nmax = 0;
+    if h_infnty_norm < 1
+      Nmax = floor(1/h_infnty_norm);
+    end
     Nmax_vector(loop_index) = Nmax;
 
     loop_index = loop_index + 1;
 end
 
+figure(1)
+stem(q, Nmax_vector, "Marker",".")
+grid on
+xlabel('q')
+ylabel('N_{max}')
+xlim([q(2) max(q)])
+yticks(0:1:Nmax_vector(2))
+xticks(q(2):90:max(q))
+if max(Nmax_vector) == 0
+    ylim([0 1])
+    yticks([0 1])
+else
+    ylim([0 Nmax_vector(2)])
+end
+
+figure(2)
 stem(q, h_infnty_norm_vector, "Marker",".")
 grid on
 xlabel('q')
@@ -109,7 +139,7 @@ ylabel('Norm_\infty')
 xlim([q(2) max(q)])
 xticks(q(2):90:max(q))
 
-function [Aaug, Baug, Caug] = get_model_augmented_matrices(mass, inertia, sampling_period)
+function [Aaug, Baug, Caug, Ad, Bd, Cd, Dd] = get_model_matrices(mass, inertia, sampling_period)
   Bv = 0.7;      % viscous friction relative to v (N/m/s)
   Bvn = 0.7;     % viscous friction relative to v n (N/m/s)
   Bw = 0.011;   % viscous friction relative to ω (N/rad/s)
@@ -159,6 +189,7 @@ function [Aaug, Baug, Caug] = get_model_augmented_matrices(mass, inertia, sampli
   Ad = robot_discrete_model.A;
   Bd = robot_discrete_model.B;
   Cd = robot_discrete_model.C;
+  Dd = robot_discrete_model.D;
 
   Aaug = ...
   [
