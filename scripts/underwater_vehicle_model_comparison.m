d377f7c26b0db82aa655d0a1d5c769625ea43f73
Mateus Menezes <mateus.meneses@fieb.org.br>
2023-12-20 00:59:16 -0300

    Improve comparison between BlueROV2 models

--- scripts/underwater_vehicle_model_comparison.m
+++ scripts/underwater_vehicle_model_comparison.m
@@ -1,81 +1,128 @@
 clc
-clear
+clear all
 
 current_script_path = fileparts(mfilename('fullpath'));
 cd(current_script_path)
 
+% Add paths so that we can use the functions from the files in the lib folder
 addpath(genpath("../lib"))
 addpath(genpath("../lib/mpc_functions"))
 addpath(genpath("../lib/chart_functions/norms"))
 addpath(genpath("../lib/dynamic_models"))
 addpath(genpath("../lib/robot_models"))
 
+% Run some scripts to load the simulation parameters
 run simulation_parameters
 run bluerov2_model
 
-dynamic_model.mass = mass;
-dynamic_model.quadratic_damping_coefficient = quadratic_damping_coefficient;
-dynamic_model.rigid_body_inertia_matrix = rigit_body_inertia_matrix;
-dynamic_model.added_mass_inertia_matrix = added_mass_inertia_matrix;
-dynamic_model.linear_damping_matrix = linear_damping_matrix;
-dynamic_model.quadratic_damping_matrix = zeros(state_vector_size, state_vector_size);
+models = [manzanilla_model; lipenitis_model; benzon_model];
+
+j = 1;
+for j=1:3
+
+dynamic_model = models(j);
+
+state_vector_size = size(dynamic_model.discrete_state_space.Ad, 1);
+states_name = {'u', 'v', 'w', 'r'};
+
 dynamic_model.gravity_vector = zeros(state_vector_size, 1);
 
 linear_x0 = zeros(state_vector_size, 1);
 nonlinear_x0 = linear_x0;
 tau = zeros(state_vector_size, 1);
-tau(1) = 8.5;
 
 integration_step = 0.001;
-simulation_time = 25.0;
+simulation_time = 60.0;
 
 linear_x = zeros(state_vector_size, simulation_time/integration_step);
 nonlinear_x = zeros(state_vector_size, simulation_time/integration_step);
+position_x = zeros(state_vector_size, simulation_time/integration_step);
 linear_x(:,1) = linear_x0;
 nonlinear_x(:,1) = nonlinear_x0;
+position_x(:,1) = nonlinear_x0;
+
 t = zeros(1, simulation_time/integration_step);
+tau = zeros(state_vector_size, simulation_time/integration_step);
 k = 1;
 
-linear_x(:,k+1) = integration_step*(A*linear_x(:,k) + B*tau) + linear_x(:,k);
+max_vel = 0.2;
+
+surge_step.begin = 5.0;
+surge_step.end = 15.0;
+surge_step.dt = integration_step;
+
+sway_step.begin = 25.0;
+sway_step.end = 35.0;
+sway_step.dt = integration_step;
+
+heave_step.begin = 40.0;
+heave_step.end = 50.0;
+heave_step.dt = integration_step;
+
+yaw_step.begin = 50.0;
+yaw_step.end = 52.0;
+yaw_step.dt = integration_step;
+
+generalized_force = [2.74; 0.0; 0.0; 0.0];
+tau = fill_tau_vector(tau, surge_step, generalized_force);
+generalized_force = [0.0; 2.74; 0.0; 0.0];
+tau = fill_tau_vector(tau, sway_step, generalized_force);
+generalized_force = [0.0; 0.0; 2.0; 0.0];
+tau = fill_tau_vector(tau, heave_step, generalized_force);
+generalized_force = [0.0; 0.0; 0.0; pi/12];
+tau = fill_tau_vector(tau, yaw_step, generalized_force);
+
+Ad = dynamic_model.discrete_state_space.Ad;
+Bd = dynamic_model.discrete_state_space.Bd;
+Cd = dynamic_model.discrete_state_space.Cd;
+
+linear_x(:,k+1) = integration_step*(Ad*linear_x(:,k) + Bd*tau(:,k)) + linear_x(:,k);
+
+start_time = cputime;
 
 while true
-  % if t(k) >= 5.0
-  %   tau(2) = 10.0;
-  % end
-  % if t(k) >= 15.0
-  %   tau(3) = 10.0;
-  % end
-  tau(4) = 0.1*sin(2*pi*0.1*integration_step*k);
-  linear_x(:,k+1) = rk4(@linear_state_space_map, linear_x0, tau, dynamic_model, integration_step);
-  nonlinear_x(:,k+1) = rk4(@nonlinear_map, nonlinear_x0, tau, dynamic_model, integration_step);
+  linear_x(:,k+1) = rk4(@linear_state_space_map, linear_x0, tau(:, k), dynamic_model, integration_step);
+  nonlinear_x(:,k+1) = rk4(@nonlinear_map, nonlinear_x0, tau(:, k), dynamic_model, integration_step);
+
   linear_x0 = linear_x(:,k+1);
   nonlinear_x0 = nonlinear_x(:,k+1);
+
   k = k + 1;
   t(k) = k*integration_step;
+
   if t(end) >= simulation_time
       break
   end
 end
 
-plot_robot_states(t, linear_x, nonlinear_x, 1, states)
+time_elapsed = cputime - start_time;
+print_section_description(['Simulation executed in ' num2str(time_elapsed) ' seconds'])
+
+figure(1)
+plot_robot_states(t, linear_x, nonlinear_x, 1, states_name)
+end
 
 function xk_plus_1 = linear_state_space_map(xk, tau, dynamic_model)
   system_inertia_matrix = dynamic_model.rigid_body_inertia_matrix + ...
-    dynamic_model.added_mass_inertia_matrix;
+    dynamic_model.added_mass_system_inertia_matrix;
   A = -system_inertia_matrix\dynamic_model.linear_damping_matrix;
   B = inv(system_inertia_matrix);
   xk_plus_1 = (A*xk + B*tau);
 end
 
 function xk_plus_1 = nonlinear_map(xk, tau, dynamic_model)
-  M = dynamic_model.rigid_body_inertia_matrix + dynamic_model.added_mass_inertia_matrix;
+  M = dynamic_model.rigid_body_inertia_matrix + dynamic_model.added_mass_system_inertia_matrix;
   C_RB = coriolis_matrix_rb(xk, dynamic_model.mass);
-  C_A = coriolis_matrix_a(xk, dynamic_model.added_mass_inertia_matrix);
+  C_A = coriolis_matrix_a(xk, dynamic_model.added_mass_system_inertia_matrix);
   C = C_RB + C_A;
-  D_v = quadratic_damping_matrix(xk, dynamic_model.quadratic_damping_coefficient);
+  D_v = quadratic_damping_matrix(xk, dynamic_model.quadratic_damping_coefficients);
   D = dynamic_model.linear_damping_matrix + D_v;
   G = dynamic_model.gravity_vector;
   xk_plus_1 = M\(tau - C*xk - D*xk);
+end
+
+function xk_plus_1 = bypass_map(xk, tau, dynamic_model)
+  xk_plus_1 = xk.*tau;
 end
 
 function C_RB = coriolis_matrix_rb(xk, mass)
@@ -115,6 +162,40 @@ function xk_plus_1 = rk4(f, xk, tau, dynamic_model, h)
   k3 = feval(f, xk + h/2*k2, tau, dynamic_model);
   k4 = feval(f, xk + h*k3, tau, dynamic_model);
   xk_plus_1 = xk + h/6*(k1 + 2*k2 + 2*k3 + k4);
+end
+
+function v = get_velocity_vec(vel, dof)
+  v = zeros(4, 1);
+  if dof == "surge"
+    v = [vel; 0.0; 0.0; 0.0];
+    return
+  end
+  if dof == "sway"
+    v = [0.0; vel; 0.0; 0.0];
+    return
+  end
+  if dof == "heave"
+    v = [0.0; 0.0; vel; 0.0];
+    return
+  end
+  if dof == "yaw"
+    v = [0.0; 0.0; 0.0; vel];
+    return
+  end
+end
+
+function vector = fill_tau_vector(tau_vector, time_instants, val_to_fill)
+  vector = tau_vector;
+
+  begin_index = time_instants.begin/time_instants.dt;
+  end_index = time_instants.end/time_instants.dt;
+
+  k = begin_index;
+
+  while k <= end_index
+    vector(:, k) = val_to_fill;
+    k = k + 1;
+  end
 end
 
 function plot_robot_states(t, linear_x, nonlinear_x, line_thickness, states_name)
