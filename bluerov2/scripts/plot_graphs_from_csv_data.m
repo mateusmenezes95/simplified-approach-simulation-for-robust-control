d3f89b4df4c1451ddfa1b46a8d2eb1478a59374c
Mateus Menezes <mateus.meneses@fieb.org.br>
2024-11-11 02:11:40 -0300

    Add graph exportation for gz sim

--- bluerov2/scripts/plot_graphs_from_csv_data.m
+++ bluerov2/scripts/plot_graphs_from_csv_data.m
@@ -1,33 +1,112 @@
 clc
 clear
 
-csv_file_path = '/home/mateus/ufba_ws/automatica-paper-scripts/bluerov2/datalogs/csv-data-test.csv';
+without_delay = "gazebo-simulation-without-delay.csv";
+with_2_samples_delay = "gazebo-simulation-with-max-delay-equal-to-2.csv";
+with_4_samples_delay = "gazebo-simulation-with-max-delay-equal-to-14.csv";
 
-ctrl_inputs = read_and_process_data(csv_file_path, [
+selection = 2; % 1 - without_delay, 2 - with_2_samples_delay, 3 - with_4_samples_delay
+
+if selection == 1
+    csv_Selected = without_delay;
+elseif selection == 2
+    csv_Selected = with_2_samples_delay;
+else
+    csv_Selected = with_4_samples_delay;
+end
+
+current_script_path = fileparts(mfilename('fullpath'));
+csv_file_path = fullfile(current_script_path, '../datalogs', 'gazebo-sim_10-11-2024', csv_Selected);
+
+%===================================================================================================
+% Plot Parameters
+%===================================================================================================
+font_size = 10;
+line_thickness = 1.5;
+y_axis_limits_offset = 0.2;
+figure_idx = 1;
+
+current_script_path = fileparts(mfilename('fullpath'));
+cd(current_script_path)
+
+% Add paths so that we can use the functions from the files in the lib folder
+addpath(genpath("."))
+addpath(genpath("../functions/plots"))
+
+[ctrl_inputs.t, ctrl_inputs.values] = read_and_process_data(csv_file_path, [
     "x__time", ...
-    "x_unconstrained_mpc_control_input_force_x", ...
-    "x_unconstrained_mpc_control_input_force_y", ...
-    "x_unconstrained_mpc_control_input_force_z", ...
-    "x_unconstrained_mpc_control_input_torque_z"
+    "x_unconstrained_mpc_mpc_data_ctrl_input_force_x", ...
+    "x_unconstrained_mpc_mpc_data_ctrl_input_force_y", ...
+    "x_unconstrained_mpc_mpc_data_ctrl_input_force_z", ...
+    "x_unconstrained_mpc_mpc_data_ctrl_input_torque_z"
 ]);
 
-robot_traj = read_and_process_data(csv_file_path, [
+[robot_traj.t, robot_traj.values] = read_and_process_data(csv_file_path, [
     "x__time", ...
-    "x_unconstrained_mpc_robot_vel_linear_x", ...
-    "x_unconstrained_mpc_robot_vel_linear_y", ...
-    "x_unconstrained_mpc_robot_vel_linear_z", ...
-    "x_unconstrained_mpc_robot_vel_angular_z"
+    "x_unconstrained_mpc_mpc_data_robot_vel_linear_x", ...
+    "x_unconstrained_mpc_mpc_data_robot_vel_linear_y", ...
+    "x_unconstrained_mpc_mpc_data_robot_vel_linear_z", ...
+    "x_unconstrained_mpc_mpc_data_robot_vel_angular_z"
 ]);
 
-desired_traj = read_and_process_data(csv_file_path, [
+[desired_traj.t, desired_traj.values] = read_and_process_data(csv_file_path, [
     "x__time", ...
-    "x_unconstrained_mpc_desired_robot_vel_linear_x", ...
-    "x_unconstrained_mpc_desired_robot_vel_linear_y", ...
-    "x_unconstrained_mpc_desired_robot_vel_linear_z", ...
-    "x_unconstrained_mpc_desired_robot_vel_angular_z"
+    "x_unconstrained_mpc_mpc_data_desired_robot_vel_linear_x", ...
+    "x_unconstrained_mpc_mpc_data_desired_robot_vel_linear_y", ...
+    "x_unconstrained_mpc_mpc_data_desired_robot_vel_linear_z", ...
+    "x_unconstrained_mpc_mpc_data_desired_robot_vel_angular_z"
 ]);
 
-function data = read_and_process_data(csv_file_path, selected_vars)
+[delay.t, delay.values] = read_and_process_data(csv_file_path, [
+    "x__time", ...
+    "x_unconstrained_mpc_mpc_data_delay_data"
+]);
+
+figure("Name", "velocities-in-body-fixed-frame")
+desired_body_fixed_vel_args.y_labels = {'u [m/s]', 'v [m/s]', 'w [m/s]', 'r [rad/s]'};
+desired_body_fixed_vel_args.y_min_offset = 0.1;
+desired_body_fixed_vel_args.y_max_offset = 0.1;
+plot_per_dof_values(desired_traj.t, desired_body_fixed_vel_args, desired_traj.values, robot_traj.values);
+
+error = abs(desired_traj.values - robot_traj.values);
+trapz(desired_traj.t, error)
+
+figure("Name", "velocities-error-in-body-fixed-frame")
+error_body_fixed_vel_args.y_labels = {'u [m/s]', 'v [m/s]', 'w [m/s]', 'r [rad/s]'};
+error_body_fixed_vel_args.y_min_offset = 0.005;
+error_body_fixed_vel_args.y_max_offset = 0.005;
+plot_per_dof_values(desired_traj.t, error_body_fixed_vel_args, error)
+
+figure("Name", "bluerov-control-signals")
+plot_generalized_forces(ctrl_inputs.t, ctrl_inputs.values', -1, '-r', line_thickness)
+
+figure("Name", "delay")
+
+% Filter the data to include only values between 10 and 20 seconds
+t_start = 10;
+t_end = 20;
+time_range = delay.t >= t_start & delay.t <= t_end;
+filtered_time = delay.t(time_range);
+filtered_values = delay.values(time_range);
+
+plot(filtered_time, filtered_values)
+xlim([t_start t_end])
+if selection == 2
+    ylim([0.0 max(delay.values)])
+else
+    ylim([0.0 max(delay.values) + 0.1])
+end
+
+ax = gca;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset;
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3) - 0.01;
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];
+
+function [t, data] = read_and_process_data(csv_file_path, selected_vars)
     % READ_AND_PROCESS_CTRL_INPUTS Reads and processes control inputs from a CSV file
     %
     %   data = READ_AND_PROCESS_CTRL_INPUTS(csv_file_path, selected_vars)
@@ -37,7 +116,7 @@ function data = read_and_process_data(csv_file_path, selected_vars)
     %   Inputs:
     %       csv_file_path - Path to the CSV file containing the data
     %       selected_vars - Cell array of selected variable names to read from the CSV file
-    %
+    %Z
     %   Output:
     %       data - Table containing the processed control inputs
     %
@@ -55,7 +134,7 @@ function data = read_and_process_data(csv_file_path, selected_vars)
     ctrl_input_opts = detectImportOptions(csv_file_path);
     ctrl_input_opts.SelectedVariableNames = selected_vars;
     data = readtable(csv_file_path, ctrl_input_opts);
-    data = rmmissing(data);
+    % data = rmmissing(data);
 
     % Process time data for control inputs
     old_values = data.x__time;
@@ -63,31 +142,7 @@ function data = read_and_process_data(csv_file_path, selected_vars)
     for i = 2:size(data, 1)
         data.x__time(i) = old_values(i) - old_values(i - 1) + data.x__time(i - 1);
     end
-end
-
-function plot_per_dof_values(t, args, desired_values, actual_values)
-	dofs = size(desired_values, 2);
-	for i = 1:dofs
-		subplot(dofs, 1, i)
-		plot(t, desired_values(:, i), 'r', 'LineWidth', 1.5)
-		ylabel(args.y_labels{i}, 'Interpreter', 'latex')
-
-		y_min = min(desired_values(:, i)) - args.y_min_offset;
-		y_max = max(desired_values(:, i)) + args.y_max_offset;
-		
-		xlim([min(t) max(t)])
-		ylim([y_min y_max])
-		grid on
-	end
-	xlabel('Time [s]')
-
-	if nargin > 3
-		for i = 1:dofs
-			subplot(dofs, 1, i)
-			hold on
-			plot(t, actual_values(:, i), 'b', 'LineWidth', 1.5)
-			hold off
-		end
-		legend('Desejado', 'Atual')
-	end
+    data = table2array(data);
+    t = data(:, 1);
+    data = data(:, 2:end);
 end
